2956f0c2b0c0595231b43a43d7b1db4aec9f0179
masterbo <seotitle@yandex.ru>
2019-01-10 18:40:36 +0500

    доработка проекта

---
+++ js/mini-js.js
@@ -0,0 +1,1 @@
+var link=document.querySelector(".feedback-button");var popup=document.querySelector(".modal__feedback");link.addEventListener("click",function(evt){evt.preventDefault();popup.classList.add("modal-show")});var close=document.querySelector(".button-close");close.addEventListener("click",function(evt){evt.preventDefault();popup.classList.remove("modal-show")})
