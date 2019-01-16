b2b9b1fa8d65057ea3f6a80c03023a147ba4e6e9
masterbo <seotitle@yandex.ru>
2019-01-16 15:08:42 +0500

    Доработка проекта - 3 попытка

--- js/js.js
+++ js/js.js
@@ -1,15 +1,19 @@
 
 var link = document.querySelector(".feedback-button");
 var popup = document.querySelector(".modal__feedback"); 
+var overlay = document.querySelector(".overlay");
+
 
 link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal-show");   
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    overlay.classList.add("overlay-show");
 });
 
 var close = document.querySelector(".button-close");    
 close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-show");
+  	evt.preventDefault();
+  	popup.classList.remove("modal-show");
+  	overlay.classList.remove("overlay-show");
 }); 
-        
+       
